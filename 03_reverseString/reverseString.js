def30b9e08d972b6ab7d83893d60922f2216b6db
Neel <neelanjan.lakshman@gmail.com>
2021-10-25 09:48:21 -0400

    Completed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
 const reverseString = function(str) {
-
+    let stringArray = str.split("");
+    let reversedArray = stringArray.reverse();
+    let reversedString = reversedArray.join("");
+    // Decrementing For Loop Method:
+    // let reversedString = '';
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     reversedString += str[i];
+    // }
+    return reversedString;
 };
 
 // Do not edit below this line
