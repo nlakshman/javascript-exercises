db326e7c91e65f2bd884109b8b1758fb823e7bb8
Neel <neelanjan.lakshman@gmail.com>
2021-10-27 09:34:08 -0400

    Passed first test in removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, argument) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === argument) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
