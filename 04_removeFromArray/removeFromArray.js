8a353d390367805d54ef42c9345fa7354dc23d15
Neel <neelanjan.lakshman@gmail.com>
2021-10-27 10:22:16 -0400

    Added comments to removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
 const removeFromArray = function() {
+    // The array is the first argument, arguments[0]
+    // Check arguments starting at second argument
     for (let i = 1; i < arguments.length; i++) {
+        // Look through all items in the array
         for (let j = 0; j < arguments[0].length; j++) {
+             // Splice out any matching arguments from the array
             if (arguments[i] === arguments[0][j]) {
                 arguments[0].splice(j, 1);
             }
