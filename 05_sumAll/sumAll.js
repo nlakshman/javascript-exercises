c46e13ff33887122d945bbb8e91106ffbbb3acd5
Neel <neelanjan.lakshman@gmail.com>
2021-10-28 09:18:06 -0400

    Completed sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    // Check constraints
+    if (typeof(firstNum) != "number" || typeof(secondNum) != "number" || isNaN(secondNum) || firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    }
+    // Determine which argument is the lower number
+    let lowNum = 0
+    let highNum = 0
+    if (firstNum < secondNum) {
+        lowNum = firstNum;
+        highNum = secondNum;
+    }
+    else {
+        lowNum = secondNum;
+        highNum = firstNum;
+    }
+    let sum = 0
+    for (let i = lowNum; i <= highNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
