f7471e4ae27cd88138ade9271ba7700d838fba3a
Neel <neelanjan.lakshman@gmail.com>
2021-10-28 10:43:08 -0400

    Completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  cTemp = Math.round(((fTemp -32) * (5/9)) * 10) / 10;
+  return cTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  fTemp = Math.round(((cTemp * (9/5)) + 32) * 10) / 10;
+  return fTemp;
 };
 
 // Do not edit below this line
