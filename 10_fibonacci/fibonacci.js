898da7a8df8b698cd6a2f30cefcbab31f440a56b
Neel <neelanjan.lakshman@gmail.com>
2021-11-10 18:06:48 -0500

    Solved fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(positiveInteger) {
+    if (positiveInteger < 1) {
+        return "OOPS"
+    }
+    // Initialize array of fibonacci sequence
+    fibArray = [1, 1]
+    // Generate fibonacci sequence until positiveInteger value
+    for (let i = 2; i < positiveInteger; i++){ 
+        fibArray.push(fibArray[i-2] + fibArray[i-1]);
+    }
+    // Return position value of positiveInteger in sequence
+    return fibArray[positiveInteger - 1]
 };
 
 // Do not edit below this line
