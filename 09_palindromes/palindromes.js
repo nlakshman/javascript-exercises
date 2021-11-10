7d2b335257eced0621cc803b259b08609c1b724b
Neel <neelanjan.lakshman@gmail.com>
2021-11-10 17:40:01 -0500

    Solved palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Make lower case and remove spaces and punctuation
+    let newString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    // Check if string characters at begining position matches that at end, and move inwards on both ends till halfway point
+    let length = newString.length
+    for (let i = 0; i < length/2; i++) {
+        if (newString[i] !== newString[length - 1 - i]) {
+                return false;
+        }
+    }
+    return true;
 };
 
+    // Alternate solution
+    // Split string into array of characters using newString.split("")
+    // Reverse the array and joing it back into string using .reverse and .join("")
+    // Compare result to newString
 // Do not edit below this line
 module.exports = palindromes;
