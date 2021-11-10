6cd76678b99aa6264742d0669afe3f96e81deb10
Neel <neelanjan.lakshman@gmail.com>
2021-11-10 15:38:22 -0500

    Completed calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(int1, int2) {
+	return int1 + int2;
 };
 
-const subtract = function() {
-	
+const subtract = function(int1, int2) {
+	return int1 - int2;
 };
 
-const sum = function() {
-	
+const sum = function(intArray) {
+	let sum = 0;
+	for (let i = 0; i < intArray.length; i++) {
+		sum += intArray[i];
+	}
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(intArray) {
+	let product = 1;
+	for (let i = 0; i < intArray.length; i++) {
+		product *= intArray[i];
+	}
+	return product;
 };
 
-const power = function() {
-	
+const power = function(int1, int2) {
+	return int1 ** int2;
 };
-
-const factorial = function() {
-	
+ 
+const factorial = function(startInt) {
+	let factorial = 1;
+	for (let i = startInt; i > 0; i--)
+		factorial *= i;
+	return factorial;
 };
 
 // Do not edit below this line
